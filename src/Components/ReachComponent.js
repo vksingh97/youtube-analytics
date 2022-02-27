90ee611d345918247a80998ed77dd01b72f6e7a5
Vivek <viveksingh.singh50@gmail.com>
2022-02-27 16:40:22 +0530

    created Reach component

---
+++ src/Components/ReachComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReachComponent = ({ data }) => {
+  return (
+    <div className="channel-reach">
+      <h2 className="reach-text">Reach and Engagment</h2>
+      <span className="reach-card" />
+    </div>
+  );
+};
+
+export default ReachComponent;
