0a43956c4cf035e0f62d11d3493b1c96f802762f
Vivek <viveksingh.singh50@gmail.com>
2022-02-27 10:38:51 +0530

    added revenue card

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import fetch from "cross-fetch";
 import "./Home.css";
 const Home = () => {
@@ -48,6 +47,28 @@ const Home = () => {
       <div className="channel-overview">
         <div className="channel-summary">
           <h2 className="summary-text">Summary</h2>
+          <span className="summary-card" />
+          <h2 className="summary-card-subscribers">Subscribers</h2>
+          <h2 className="summary-card-views">Views</h2>
+          <h2 className="summary-card-revenue">Revenue</h2>
+          <h2 className="summary-card-subscribers-count">
+            {data ? data.summary.subscribers : null}
+          </h2>
+          <h2 className="summary-card-views-count">
+            {data ? data.summary.views : null}
+          </h2>
+          <h2 className="summary-card-revenue-count">
+            &#x20b9;
+            {data ? data.summary.revenue : null}
+          </h2>
+        </div>
+        <div className="channel-revenue">
+          <h2 className="revenue-text">Revenue</h2>
+          <span className="revenue-card" />
+          <h2 className="estimated-revenue-text">Estimated Revenue</h2>
+          <h2 className="estimated-revenue-value">
+            {data ? data.revenueDetails.estimatedRevenueTrend.value : null}
+          </h2>
         </div>
       </div>
     </div>
